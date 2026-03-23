80ba9d258eecff98116bcfb8b836d945dc610b91
osndl.nikhilsk <nikhilkarakoti2017@gmail.com>
2026-03-23 16:18:18 +0530

    Fragment Layout of express and metro Layout.

--- app/src/main/java/com/example/buildjetpackcomposeapplicationexamples/ui/DifferentUiComponents/MainActivityComponents.kt
+++ app/src/main/java/com/example/buildjetpackcomposeapplicationexamples/ui/DifferentUiComponents/MainActivityComponents.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -12,43 +13,78 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.buildjetpackcomposeapplicationexamples.ui.theme.BuildJetpackComposeApplicationExamplesTheme
+
 
 class MainActivityComponents : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            setupExpressAndMetroUI()
+//            setupExpressAndMetroUI()
+        }
+    }
+}
+@Composable
+fun setupExpressAndMetroUI(navController: NavHostController) {
+    var isClick by rememberSaveable { mutableStateOf(true) }
+    Row {
+        Button(
+            onClick = {
+                isClick = true
+                navController.navigate("express")
+            }, colors = ButtonDefaults.buttonColors(
+                containerColor = if (isClick) Color.Black else Color.Gray,
+                contentColor = if (isClick) Color.White else Color.White
+            ), modifier = Modifier.weight(1f)
+                .height(60.dp).padding(5.dp)
+        ) {
+            Text("Express")
+        }
+        Spacer(Modifier.padding(5.dp))
+        Button(onClick = {
+            isClick = false
+            navController.navigate("metro")
+        }, colors = ButtonDefaults.buttonColors(
+            containerColor = if (!isClick) Color.Black else Color.Gray,
+            contentColor = if (!isClick) Color.White else Color.White
+        ), modifier = Modifier.weight(1f)
+            .height(60.dp).padding(5.dp)) {
+            Text("Metro")
+        }
+    }
+
+}
+
+@Composable
+fun AppNavigation(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = "express") {
+        composable("express") {
+            expressLayout()
+        }
+        composable("metro") {
+            metroScreen()
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun setupExpressAndMetroUI() {
-    var isClick by remember { mutableStateOf(false) }
-    Row {
-        Button(
-            onClick = {
-                isClick = true
-            }, colors = ButtonDefaults.buttonColors(
-                containerColor = if (!isClick) Color.Red else Color.Blue,
-                contentColor = if (!isClick) Color.White else Color.Black
-            )
-        ) {
-            Text("Express")
-        }
-        Spacer(Modifier.padding(10.dp))
-        Button(onClick = {
-            isClick = false
-        }) {
-            Text("Metro")
-        }
-    }
+fun expressLayout() {
+    Text("Express Layout")
+}
 
+@Composable
+private fun metroScreen() {
+    Text("metro Layout")
 }
